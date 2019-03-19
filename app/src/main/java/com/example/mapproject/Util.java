82aed9b93a07d57437ffded33c1c74e6452dc971
Kawaldeep Kaur <kawaldeep16894@gmail.com>
2019-03-19 10:47:55 +0530

    Update Util.java

--- app/src/main/java/com/example/mapproject/Util.java
+++ app/src/main/java/com/example/mapproject/Util.java
@@ -1,8 +1,8 @@
 package com.example.mapproject;
 
 public class Util {
-    static String url = "http://202.164.39.172:2345/test/acm/";
-    static String url1="http://192.168.42.46/GovtOfficials/";
+   
+  
     static String url2 ="http://suraj1993-001-site1.ftempurl.com/phpfiles/";
 
     public static final String Districts = url2+"Districts.php";
@@ -11,12 +11,5 @@ public class Util {
     public static final String LatLong = url2+"latlong.php";
     public static final String getdetails = url2+"getdetails.php";
 
-    /*public static final String Districts = url+"GovtOfficials/Districts.php";
-    public static final String Blocks = url1+"GovtOfficials/blocks.php";
-    public static final String Villages = url1+"GovtOfficials/villages.php";
-    public static final String LatLong = url1+"GovtOfficials/latlong.php";*/
-
-    //  public static final String Insert = url1+"GovtOfficials/State.php";
-    //  public static final String LatLong = "http://192.168.42.46/local.php";
 
 }
